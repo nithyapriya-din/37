25df830dbc3d94c18fdbd67cc453108ac40e3768
nithyapriya-din <142155664+nithyapriya-din@users.noreply.github.com>
2024-01-30 23:42:39 +0530

    student.js

---
+++ models/models/student.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const studentSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    mentor: { type: mongoose.Schema.Types.ObjectId, ref: 'Mentor' },
+});
+module.exports = mongoose.model('Student', studentSchema);
+0
