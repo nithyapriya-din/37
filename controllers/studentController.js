bec0cdaa2d1bb4d5f065313c7066eaf059b12be0
nithyapriya-din <142155664+nithyapriya-din@users.noreply.github.com>
2024-01-30 23:49:24 +0530

    studentController.js

---
+++ controllers/studentController.js
@@ -0,0 +1,22 @@
+const Student = require('../models/student');
+const Mentor = require('../models/mentor');
+
+exports.createStudent = async (req, res) => {
+    
+};
+
+exports.assignMentor = async (req, res) => {
+    
+};
+
+exports.getUnassignedStudents = async (req, res) => {
+    
+};
+
+exports.getStudentsByMentor = async (req, res) => {
+
+};
+
+exports.getPreviousMentor = async (req, res) => {
+    
+};
