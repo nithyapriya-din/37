cc269e79a9a628dc4caf2711db09d666ae3d1dfc
nithyapriya-din <142155664+nithyapriya-din@users.noreply.github.com>
2024-01-30 23:41:44 +0530

    mentor.js

---
+++ models/mentor.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+const mentorSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+});
+module.exports = mongoose.model('Mentor', mentorSchema);
